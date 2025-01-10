7bd3ee32ec998c99b88c3122bebdbb4a098383b8
Sergio Mendoza Méndez <zergiomendoza96@hotmail.com>
2025-01-10 16:15:24 -0500

    se crea la nueva rama y se agregan detalles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,4 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import Login from './Login';
-// import Register from './Register';
-// import PrincipalPage from './PrincipalPage';
-// import '../styles.css';
-// function App() {
-//   const isAuthenticated = localStorage.getItem('email');  // Verifica si el usuario está logueado
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Ruta para login */}
-//         <Route path="/login" element={<Login />} />
-//         {/* Ruta para registro */}
-//         <Route path="/register" element={<Register />} />
-//         {/* Ruta para la página principal - Solo accesible si el usuario está logueado */}
-//         <Route
-//           path="/main"
-//           element={isAuthenticated ? <PrincipalPage /> : <Navigate to="/login" />}
-//         />
-//         {/* Ruta por defecto */}
-//         <Route path="/" element={<Login />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
 
-// export default App;
 import React from 'react';
 import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Login';
